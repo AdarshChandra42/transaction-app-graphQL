7603c29f724ae11ff40b1b50faf6a3b796e91875
Adarsh Chandra <84859834+AdarshChandra42@users.noreply.github.com>
2025-04-26 12:25:51 +0530

    Update passport.config.js

--- backend/passport/passport.config.js
+++ backend/passport/passport.config.js
@@ -7,7 +7,7 @@ import { GraphQLLocalStrategy } from "graphql-passport";
 export const configurePassport = async () => {
 	passport.serializeUser((user, done) => {
 		console.log("Serializing user");
-		done(null, user.id);
+		done(null, user.id); // store only the user's ID into the session
 	});
 
 	passport.deserializeUser(async (id, done) => {
